af61fce7a36f22600de4bf8e26a90f8e38e6e213
Green081081 <blessededet94@gmail.com>
2023-01-05 16:27:40 +0100

    cahnges made to the JSON file

--- JS/data.js
+++ JS/data.js
@@ -1,4 +1,5 @@
 const requestUrl = "https://greenz081081.github.io/My-YouTube-Tutorials/JSON/data.json";
+// const requestUrl = "https://github.com/Greenz081081/My-YouTube-Tutorials/blob/main/data.json"
 
 async function getProducts () {
     let response = await fetch(requestUrl);
